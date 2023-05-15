66c83d836e7d26e04e928c8005788847f103a411
RoyStorey <imlejiii@gmail.com>
2023-05-15 00:29:59 -0400

    Hamburger and styling

--- src/components/home.js
+++ src/components/home.js
@@ -1,6 +1,10 @@
 import React from "react";
 import Footer from "../functional-components/footer";
 import "../css/home.css"
+import "../css/hamburger.css"
+import toggleHamburgerMenu from "../functions/toggle-hamburger-menu";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faHamburger, faCheck, faX, faCoffee, faLineChart, faChessBoard, faDashboard } from '@fortawesome/free-solid-svg-icons'
 // import BonkoImage from "../media/bonko.png";
 
 class Home extends React.Component {
@@ -15,20 +19,14 @@ class Home extends React.Component {
         <div className="home-body-container">
           <div className="home-content-container">
             <div className="header-container home-account-menu-header">
-              <div id="hamburger-icon"></div>
+              <div className="hamburger-icon-container">
+                <FontAwesomeIcon icon={faHamburger} onClick={toggleHamburgerMenu} className="hamburger-icon interactable" />
+              </div>
               <div className="home-account-container">
                 <div className="home-account-icon-container"></div>
-                <div className="home-account-name-container">
-                  <h3>bonko admin<i className="arrow down"></i></h3>
+                <div className="home-account-name-container interactable">
+                  <h3>fname lname<i className="arrow down"></i></h3>
                 </div>
-              </div>
-            </div>
-            <div className="header-container">
-              <div className="home-menu-options-container">
-                <h3>Dashboard Overlaying Users Calendar</h3>
-                <h3>Boards</h3>
-                <h3>Metrics</h3>
-                <h3>???</h3>
               </div>
             </div>
             <div className="home-panel-container">
@@ -38,33 +36,91 @@ class Home extends React.Component {
                 </div>
                 <div className="home-calendar-container">
                   <div className="home-calendar">
-                    <h3>calendar</h3>
+                    <h5>calendar</h5>
                   </div>
                 </div>
               </div>
               <div className="home-panel">
                 <div className="home-to-do-container">
                   <div className="home-to-do-header">
-                    <h3><i className="arrow left"></i>yesterday</h3>
-                    <div className="home-to-do-header-text-container">
+                    <h3 className="interactable"><i className="arrow left"></i>yesterday</h3>
+                    <div className="home-to-do-header-text-container interactable">
                       <h3>Today's todo</h3>
                       <h3>05/10/2023</h3>
                     </div>
-                    <h3>tomorrow<i className="arrow right"></i></h3>
+                    <h3 className="interactable">tomorrow<i className="arrow right"></i></h3>
                   </div>
                   <div className="home-to-do-body">
                     <div className="home-to-do-list-container">
                       <ul>
-                        <li>item</li>
-                        <li>item</li>
-                        <li>item</li>
-                        <li>item</li>
-                        <li>item</li>
+                        <div className="home-to-do-list-item-container">
+                          <li className="home-to-do-list-item-name interactable">item 1</li>
+                          <li> 0/5 completed</li>
+                          <div className="home-to-do-list-item-checkbox-container">
+                            <FontAwesomeIcon icon={faCheck} className="list-item-check-icon" />
+                            <FontAwesomeIcon icon={faX} className="list-item-x-icon" />
+                          </div>
+                        </div>
+                        <div className="home-to-do-list-item-container">
+                          <li className="home-to-do-list-item-name interactable">item 2</li>
+                          <li>0/5 completed</li>
+                          <div className="home-to-do-list-item-checkbox-container">
+                            <FontAwesomeIcon icon={faCheck} className="list-item-check-icon" />
+                            <FontAwesomeIcon icon={faX} className="list-item-x-icon" />
+                          </div>
+                        </div>
+                        <div className="home-to-do-list-item-container">
+                          <li className="home-to-do-list-item-name interactable">item 3</li>
+                          <li>1/3 completed</li>
+                          <div className="home-to-do-list-item-checkbox-container">
+                            <FontAwesomeIcon icon={faCheck} className="list-item-check-icon" />
+                            <FontAwesomeIcon icon={faX} className="list-item-x-icon" />
+                          </div>
+                        </div>
+                        <div className="home-to-do-list-item-container">
+                          <li className="home-to-do-list-item-name interactable">item 4</li>
+                          <li>0/9 completed</li>
+                          <div className="home-to-do-list-item-checkbox-container">
+                            <FontAwesomeIcon icon={faCheck} className="list-item-check-icon" />
+                            <FontAwesomeIcon icon={faX} className="list-item-x-icon" />
+                          </div>
+                        </div>
+                        <div className="home-to-do-list-item-container">
+                          <li className="home-to-do-list-item-name interactable">item 5</li>
+                          <li>4/5 completed</li>
+                          <div className="home-to-do-list-item-checkbox-container">
+                            <FontAwesomeIcon icon={faCheck} className="list-item-check-icon" />
+                            <FontAwesomeIcon icon={faX} className="list-item-x-icon" />
+                          </div>
+                        </div>
                       </ul>
                     </div>
                   </div>
                 </div>
               </div>
+            </div>
+          </div>
+        </div>
+        <div className="hamburger-menu-container" id="hamburger-menu-container">
+          <div className="hamburger-menu-header-container">
+            <h1>hi fname</h1>
+          </div>
+          <div className="hamburger-menu-body-container">
+            <div className="hamburger-menu-body-item">
+              <FontAwesomeIcon icon={faDashboard} />
+              <h3 className="interactable">dashboard overlaying users calendar</h3>
+            </div>
+            <div className="hamburger-menu-body-item">
+              <FontAwesomeIcon icon={faChessBoard} />
+              <h3 className="interactable">boards</h3>
+            </div>
+            <div className="hamburger-menu-body-item">
+              <FontAwesomeIcon icon={faLineChart} />
+              <h3 className="interactable">metrics</h3>
+            </div>
+            <div className="hamburger-menu-body-item">
+              <FontAwesomeIcon icon={faCoffee} />
+              <h3 className="interactable">???</h3>
             </div>
           </div>
         </div>
---
+++ src/functions/toggle-hamburger-menu.js
@@ -0,0 +1,6 @@
+function toggleHamburgerMenu() {
+  var hamburgerMenu = document.getElementById('hamburger-menu-container')
+  hamburgerMenu.classList.toggle('hamburger-active')
+}
+
+export default toggleHamburgerMenu
